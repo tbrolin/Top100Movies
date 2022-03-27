9eceb7a1ba359c74bc914598cca64ee9e4d607b2
Tobias Brolin <tobiasbrolin@gmail.com>
2022-03-27 16:32:52 +0200

    Now running from express

--- server/graphql.js
+++ server/graphql.js
@@ -1,6 +1,7 @@
 /* eslint-disable unicorn/prefer-module */
 const process = require('process')
 const express = require('express')
+const path = require('path')
 const { graphqlHTTP } = require('express-graphql')
 const { buildSchema } = require('graphql')
 const cors = require('cors')
@@ -9,6 +10,7 @@ const axios = require('axios')
 
 const MDB_URL = 'https://api.themoviedb.org/3/discover/movie'
 const API_KEY = process.env?.API_KEY
+const PORT = process.env.PORT || 4000
 if (!API_KEY) {
   throw new Error(
     'ERROR: You have to to set the env variable API_KEY to your TMDB api key to be able to communicate with TMDB'
@@ -66,5 +68,11 @@ app.use(
   })
 )
 
-app.listen(4000)
-console.info('GraphQL server up and running. (http://localhost:4000/graphql')
+app.use(express.static(path.join(__dirname, '../build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(PORT)
+console.info('Server running...')
